f01d6e160639765539f205af1f671000f2f75ce9
rmkw <creditozona1@gmail.com>
2026-06-17 14:59:17 -0600

    Agrega entidades de comentarios_pp

---
+++ src/main/java/mx/org/inegi/sistemacaptura/armonizacion/entity/comentarios_pp/comentarios_pp_armo_dto.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.org.inegi.sistemacaptura.armonizacion.entity.comentarios_pp;
+
+/**
+ *
+ * @author LUIS.CASTANEDAL
+ */
+
+public class comentarios_pp_armo_dto {
+
+    private String acronimo;
+    private String comentarioS;
+
+    public String getAcronimo() {
+        return acronimo;
+    }
+
+    public void setAcronimo(String acronimo) {
+        this.acronimo = acronimo;
+    }
+
+    public String getComentarioS() {
+        return comentarioS;
+    }
+
+    public void setComentarioS(String comentarioS) {
+        this.comentarioS = comentarioS;
+    }
+}
---
+++ src/main/java/mx/org/inegi/sistemacaptura/armonizacion/entity/comentarios_pp/comentarios_pp_armo_enty.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.org.inegi.sistemacaptura.armonizacion.entity.comentarios_pp;
+
+/**
+ *
+ * @author LUIS.CASTANEDAL
+ */
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "comentarios_pp", schema = "armonizacion")
+public class comentarios_pp_armo_enty {
+
+    @Id
+    @Column(name = "acronimo", nullable = false, length = 50)
+    private String acronimo;
+
+    @Column(name = "comentario_s", columnDefinition = "TEXT")
+    private String comentarioS;
+
+    public String getAcronimo() {
+        return acronimo;
+    }
+
+    public void setAcronimo(String acronimo) {
+        this.acronimo = acronimo;
+    }
+
+    public String getComentarioS() {
+        return comentarioS;
+    }
+
+    public void setComentarioS(String comentarioS) {
+        this.comentarioS = comentarioS;
+    }
+}
